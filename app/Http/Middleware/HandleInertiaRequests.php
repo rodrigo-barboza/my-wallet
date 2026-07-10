0436eb7944d863ff38697f73aec98f4cb8489b8f
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2026-07-10 00:57:38 -0300

    chore: configure Ziggy to expose only needed routes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -37,7 +38,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            //
+            'ziggy' => fn () => (new Ziggy)->toArray(),
         ];
     }
 }
--- config/ziggy.php
+++ config/ziggy.php
@@ -3,8 +3,12 @@
 return [
     'only' => [
         'home',
+        'login',
+        'login.store',
         'register',
         'register.store',
+        'dashboard',
+        'logout',
         'verification.notice',
         'verification.verify',
         'verification.send',
