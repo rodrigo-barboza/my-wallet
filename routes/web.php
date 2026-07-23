e4933531bc10fe290a885245b958c26b246ace2d
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2026-07-22 22:49:09 -0300

    refactor: remove Welcome.vue and update home route to redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@ use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PreferencesController;
 use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::get('/', fn() => to_route('dashboard'))->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'create'])->name('login');
