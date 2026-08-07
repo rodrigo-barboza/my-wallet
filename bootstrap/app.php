b7b8da8d761c17204fea228a0246a40c5976a238
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2026-08-07 01:56:31 -0300

    fix: add missing trust proxies configuration in middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,24 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('notifications:send')->dailyAt('08:00');
     })
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: [
+            '173.245.48.0/20',
+            '103.21.244.0/22',
+            '103.22.200.0/22',
+            '103.31.4.0/22',
+            '141.101.64.0/18',
+            '108.162.192.0/18',
+            '190.93.240.0/20',
+            '188.114.96.0/20',
+            '197.234.240.0/22',
+            '198.41.128.0/17',
+            '162.158.0.0/15',
+            '104.16.0.0/13',
+            '104.24.0.0/14',
+            '172.64.0.0/13',
+            '131.0.72.0/22',
+        ]);
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
